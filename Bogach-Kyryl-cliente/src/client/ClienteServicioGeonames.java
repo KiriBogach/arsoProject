ac3c65affe54426908a70fe97bb16cea262a9433
Victorni <Victorni@DESKTOP-I5EOCPO>
2019-04-11 16:40:58 +0200

    Finalizado cliente REST

--- Bogach-Kyryl-cliente/src/client/ClienteServicioGeonames.java
+++ Bogach-Kyryl-cliente/src/client/ClienteServicioGeonames.java
@@ -1,5 +1,7 @@
 package client;
 
+import java.util.List;
+
 import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.client.Client;
@@ -7,8 +9,10 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.WebResource.Builder;
 
+import servicio.controlador.CiudadesFavoritas;
 import servicio.controlador.ListadoCiudades;
 import servicio.model.CiudadGeoNames;
+import servicio.tipos.Ciudad;
 
 
 public class ClienteServicioGeonames {
@@ -23,7 +27,6 @@ public class ClienteServicioGeonames {
 
 		// getResultadosBusqueda
 		WebResource recurso = cliente.resource(URL_SERVICIO + path);
-		System.out.println(URL_SERVICIO + path);
 		Builder builder = recurso.queryParam("busqueda", "cartagena").accept(MediaType.APPLICATION_XML);
 		ClientResponse respuesta = builder.method("GET", ClientResponse.class);
 		ListadoCiudades listado = respuesta.getEntity(ListadoCiudades.class);
@@ -33,5 +36,65 @@ public class ClienteServicioGeonames {
 		for (CiudadGeoNames ciudad : listado.getCiudades()) {
 			System.out.println(ciudad.getNombre());
 		}
+		System.out.println("--------------------------------------------");
+		recurso = cliente.resource(URL_SERVICIO + path + "/2520058");
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("GET", ClientResponse.class);
+		Ciudad ciudad = respuesta.getEntity(Ciudad.class);
+		System.out.println("Código de retorno: " + respuesta.getStatus());
+
+		System.out.println("ciudad: " + ciudad.getNombre());
+		System.out.println("pais: " + ciudad.getPais());
+		System.out.println("wikipedia: " + ciudad.getWikipedia());
+		System.out.println("bdpedia: " + ciudad.getBdpedia());
+		
+		recurso = cliente.resource(URL_SERVICIO + path + "/favoritas");
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("POST", ClientResponse.class);
+		List<String> favCodigos = respuesta.getHeaders().get("location");
+		String favCodigo = favCodigos.get(0);
+		System.out.println(respuesta.getStatus());
+		
+		recurso = cliente.resource(favCodigo + "/2520058");
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("PUT", ClientResponse.class);
+		System.out.println(respuesta.getStatus());
+		System.out.println("--------------------------------------------");
+		
+		recurso = cliente.resource(favCodigo);
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("GET", ClientResponse.class);
+		CiudadesFavoritas favs = respuesta.getEntity(CiudadesFavoritas.class);
+		System.out.println(favs.getId());
+		favs.getCiudades().forEach(c -> System.out.println(c));
+		System.out.println("--------------------------------------------");
+		
+		recurso = cliente.resource(favCodigo + "/2520058");
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("DELETE", ClientResponse.class);
+		System.out.println(respuesta.getStatus());
+		System.out.println("--------------------------------------------");
+		
+		recurso = cliente.resource(favCodigo);
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("GET", ClientResponse.class);
+		favs = respuesta.getEntity(CiudadesFavoritas.class);
+		System.out.println(favs.getId());
+		favs.getCiudades().forEach(c -> System.out.println(c));
+		System.out.println("--------------------------------------------");
+		
+		recurso = cliente.resource(favCodigo);
+		builder = recurso.accept(MediaType.APPLICATION_XML);
+		respuesta = builder.method("DELETE", ClientResponse.class);
+		System.out.println(respuesta.getStatus());
+		System.out.println("--------------------------------------------");
+		
+		recurso = cliente.resource(URL_SERVICIO + path);
+		builder = recurso.queryParam("busqueda", "cartagena").accept(MediaType.APPLICATION_JSON);
+		respuesta = builder.method("GET", ClientResponse.class);
+		String content = respuesta.getEntity(String.class);
+		System.out.println("Código de retorno: " + respuesta.getStatus());
+
+		System.out.println(content);
 	}
 }
